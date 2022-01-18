46eb9ce8d3e98c9531707d5a9ee6faa74d79aa19
Naebaim <lyd0223@gmail.com>
2022-01-18 13:11:54 +0900

    Add IP Filter

--- Project/GameServerNet/PreCompile.h
+++ Project/GameServerNet/PreCompile.h
@@ -1,2 +1,13 @@
 #pragma once
 
+
+#include <GameServerBase/PreCompile.h>
+
+#include <GameServerBase/GameServerObjectBase.h>
+#include <GameServerBase/GameServerNameObject.h>
+#include <GameServerBase/GameServerQueue.h>
+#include <GameServerBase/GameServerDebug.h>
+//#include <GameServerBase/GameServerUnique.h>
+
+#include "EnumInfo.h"
+//#include "TypeDefine.h"
