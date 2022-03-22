20b0ab791093089ab1152bb81da5580e5af48021
Naebaim <lyd0223@gmail.com>
2022-03-22 21:36:45 +0900

    Revert "Add DBConnecter"
    This reverts commit 732d795bffc5939d21ae2c84ae82b9b78b2bfe33.

--- PorjectCode/GameServerNet/DBConnecter.cpp
+++ PorjectCode/GameServerNet/DBConnecter.cpp
@@ -4,12 +4,7 @@
 
 DBConnecter::DBConnecter()
 {
-	m_MYSQL = nullptr;
-	m_Host = "";
-	m_ID = "";
-	m_PW = "";
-	m_SchemaName = "";
-	m_Port = NULL;
+
 }
 
 DBConnecter::~DBConnecter()
@@ -22,58 +17,11 @@ DBConnecter::DBConnecter(DBConnecter&& _Other) noexcept
 
 }
 
-std::unique_ptr<DBStmt> DBConnecter::CreateDBStmt(std::string_view _DBQuery)
+bool DBConnecter::IsValid()
 {
-
-}
-
-
-bool DBConnecter::Connect()
-{
-	if (m_MYSQL != nullptr) // 이미 연결되어있는상태.
+	if (this == nullptr)
 	{
-		return true;
-	}
-	
-	m_MYSQL = mysql_init(nullptr);
-	if (m_MYSQL == nullptr)
-	{
-		GameServerDebug::AssertDebugMsg("mysql init failed");
 		return false;
 	}
-	
-	//mysql 문자 인코딩방식설정
-	mysql_options(m_MYSQL, MYSQL_SET_CHARSET_NAME, "utf8");
-
-	//MysqlRealConnect
-	MYSQL* MYSQLHandle = mysql_real_connect(m_MYSQL, m_Host.c_str(), m_ID.c_str(), m_PW.c_str(), m_SchemaName.c_str(), m_Port, nullptr, CLIENT_MULTI_RESULTS);
-	
-	if (m_MYSQL != MYSQLHandle)
-	{
-		GameServerDebug::AssertDebugMsg("mysql real connect failed" + GetLastError());
-		return false;
-	}
-
-	//Mysql 자동복구옵션
-	bool isConnected = true;
-	mysql_options(m_MYSQL, MYSQL_OPT_RECONNECT, &IsConnected);
-
 	return true;
-}
-
-bool DBConnecter::Connect(const std::string& _Host, const std::string& _ID,
-	const std::string& _PW, const std::string& _SchemaName, unsigned int _Port)
-{
-	m_Host = _Host;
-	m_ID = _ID;
-	m_PW = _PW;
-	m_SchemaName = _SchemaName;
-	m_Port = _Port;
-
-	return Connect();
-}
-
-std::string DBConnecter::GetLastErrorStr()
-{
-	return mysql_error(m_MYSQL);
 }
--- PorjectCode/GameServerNet/DBConnecter.h
+++ PorjectCode/GameServerNet/DBConnecter.h
@@ -1,10 +1,11 @@
 #pragma once
 
 /*
-Usage		:  DB연결담당
-Category	:  
-Description :  DB연결
+Usage		:  모든 클래스 기본.  
+Category	:  Object 
+Description :  오브젝트 상속용.
 */
+
 class DBConnecter
 {
 public: //Default
@@ -12,45 +13,16 @@ public: //Default
 	~DBConnecter();
 
 	DBConnecter(const DBConnecter& _Other) = delete;
-	DBConnecter(DBConnecter&& _Other)		noexcept = delete;
+	DBConnecter(DBConnecter&& _Other)		noexcept;
+
+protected:
 	DBConnecter& operator=(const DBConnecter& _Other) = delete;
 	DBConnecter& operator=(DBConnecter&& _Other)		= delete;
 
 private: //Member Value
-	MYSQL* m_MYSQL;
-
-	std::string m_Host;
-	std::string m_ID;
-	std::string m_PW;
-	std::string m_SchemaName;
-	unsigned int m_Port;
 	
-public: //Get,Set function
-	inline bool IsConnected()
-	{
-		return m_MYSQL != nullptr;
-	}
-
+	
 public:  //Member Function
-	std::unique_ptr<DBStmt> CreateDBStmt(std::string_view _DBQuery);
-	
-	bool Connect();
-	bool Connect(const std::string& _Host, const std::string& _ID, 
-		const std::string& _PW, const std::string& _SchemaName, unsigned int _Port);
-
-	std::string GetLastErrorStr();
-	
+	bool IsValid();
 };
 
-
-
-
-class DBStmt
-{
-
-};
-
-class DBStmtResult
-{
-
-};
